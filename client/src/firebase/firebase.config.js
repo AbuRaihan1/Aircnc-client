7967907c93263fff62143c67bfe9386fdc1db3b2
Abu Raihan <raihanahmed01973@gmail.com>
2023-07-30 06:02:24 -0500

    added signup

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,9 +1,13 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  // COnfig code goes here
-}
+  apiKey: "AIzaSyA0Y6LwzQyA5nFdEyvxN6kWgXJsSXRf5hs",
+  authDomain: "email-authenticaion.firebaseapp.com",
+  projectId: "email-authenticaion",
+  storageBucket: "email-authenticaion.appspot.com",
+  messagingSenderId: "1045931568311",
+  appId: "1:1045931568311:web:f2e9a4e1ca46246ed27132",
+};
+const app = initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig)
-
-export default app
+export default app;
